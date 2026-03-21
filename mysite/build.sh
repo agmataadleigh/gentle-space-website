600685b5bdabd3043f8840a48d74fbefc0628b15
agmataadleigh <agmataadleigh@gmail.com>
2026-03-20 22:58:32 -0700

    Moving everything to the root for a clean build

--- mysite/build.sh
+++ mysite/build.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
-# Add the two dots and slash here!
-pip install -r ../requirements.txt
+pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
